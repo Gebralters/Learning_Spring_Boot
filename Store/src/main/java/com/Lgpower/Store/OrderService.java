3fede32c1a35574ea23ac5d806c2518ed15850b6
Letheba Kaka <lethebagebralters@gmail.com>
2025-09-21 19:09:09 +0200

    update

---
+++ Store/src/main/java/com/Lgpower/Store/OrderService.java
@@ -0,0 +1,15 @@
+package com.Lgpower.Store;
+
+public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+
+    public void placeOrder() {
+        var paymentService = new StripePaymentService();
+        paymentService.processPayment(1000);
+    }
+}
